bc09155349b98fd807e24f17b21413e91814bb2f
JayramNanoskar <jayram.n978@gmail.com>
2021-07-03 20:12:48 +0530

    Resource life cycle: making resource singleton

--- src/main/java/com/jayram/rest/advance/MyResorce.java
+++ src/main/java/com/jayram/rest/advance/MyResorce.java
@@ -1,16 +1,29 @@
 package com.jayram.rest.advance;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/test")
+@Singleton
 public class MyResorce {
 
+	private int count;
+	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod(){
 		return "It works!";
 	}
+	
+	@GET
+	@Path("/count")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String testMethod2(){ //Default resource instance scope is Request scope, but we can change it to Singleton resource scope. So JAX-RS held on that same resource instance for every request till server is running.
+		count = count + 1;
+		return "It works! This method was called "+count+ " time(s)";
+	}
 }
+
