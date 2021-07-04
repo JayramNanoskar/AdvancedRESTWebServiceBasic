1e6eb80aecc9076ce05f3eb3d67334ee0404ae58
JayramNanoskar <jayram.n978@gmail.com>
2021-07-04 12:31:36 +0530

    Refactoring code: using recommended way of managing target instances

--- src/main/java/com/jayram/rest/advance/client/RestApiClient.java
+++ src/main/java/com/jayram/rest/advance/client/RestApiClient.java
@@ -2,6 +2,7 @@ package com.jayram.rest.advance.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import com.jayram.rest.messenger.model.Message;
@@ -12,7 +13,11 @@ public class RestApiClient {
 		
 		Client client = ClientBuilder.newClient();
 		
-		Message message = client.target("http://localhost:8080/advanced-jaxrs/webapi/messages/1") //Using Fluent API here i.e. chaining of methods one after other till we get object we want.
+		WebTarget baseTarget = client.target("http://localhost:8080/advanced-jaxrs/webapi/");
+		WebTarget messageTarget = baseTarget.path("messages");
+		WebTarget singleMessageTarget = messageTarget.path("{messageId}");
+		
+		Message message = singleMessageTarget.resolveTemplate("messageId", "1") //Using Fluent API here i.e. chaining of methods one after other till we get object we want.
 									.request(MediaType.APPLICATION_JSON)
 									.get(Message.class); //For troubleshooting point of view, when getting error to convert stream to the specified instance of the class i.e at the time of trying to get object out of response(Unmarshalling), we can specify String.class so we can get actual payload to examine. 
 		System.out.println(message.getMessage());
